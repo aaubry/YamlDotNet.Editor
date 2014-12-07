caa4a5b6728f988d4f93d11dac6028704a274532
Antoine Aubry <aaubry@gmail.com>
2014-12-07 21:10:56 +0000

    Fix bug in build script.

--- prebuild.ps1
+++ prebuild.ps1
@@ -3,5 +3,7 @@
 $buildNumber = ($env:APPVEYOR_BUILD_NUMBER, "0" -ne $null)[0]
 $version = Get-VersionFromTag
 
-Patch-Xml "YamlDotNetEditor\source.extension.vsixmanifest" $version $buildNumber "/vsx:PackageManifest/vsx:Metadata/vsx:Identity/@Version" @{ vsx = "http://schemas.microsoft.com/developer/vsx-schema/2011" }
-Patch-AssemblyInfo "YamlDotNetEditor\Properties\AssemblyInfo.cs" $version $buildNumber
+Update-AppveyorBuild -Version "$version.$buildNumber"
+
+Patch-Xml "YamlDotNetEditor\source.extension.vsixmanifest" $version "/vsx:PackageManifest/vsx:Metadata/vsx:Identity/@Version" @{ vsx = "http://schemas.microsoft.com/developer/vsx-schema/2011" }
+Patch-AssemblyInfo "YamlDotNetEditor\Properties\AssemblyInfo.cs" $version
