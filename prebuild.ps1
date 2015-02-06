6da92675af2a4b351d6790af9b87765a34ba42dc
Antoine Aubry <aaubry@gmail.com>
2015-02-06 19:38:18 +0000

    Also use APPVEYOR_BUILD_NUMBER when not building in the release branch.

--- prebuild.ps1
+++ prebuild.ps1
@@ -5,5 +5,9 @@ $version = Get-VersionFromTag
 
 Update-AppveyorBuild -Version "$version.$buildNumber"
 
+if ($env:APPVEYOR_REPO_BRANCH -ne "release") {
+	$version = "$version.$buildNumber"
+}
+
 Patch-Xml "YamlDotNetEditor\source.extension.vsixmanifest" $version "/vsx:PackageManifest/vsx:Metadata/vsx:Identity/@Version" @{ vsx = "http://schemas.microsoft.com/developer/vsx-schema/2011" }
 Patch-AssemblyInfo "YamlDotNetEditor\Properties\AssemblyInfo.cs" $version
